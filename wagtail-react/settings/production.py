119e5fd8bcf5cfbce2c2af5b1b0aa17bad1d1a97
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-06-10 23:52:55 +0900

    init personal-porfolio

--- wagtail-react/settings/production.py
+++ wagtail-react/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 from decouple import config
-
+import dj_database_url
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['personal-porfolio.herokuapp.com']
@@ -14,14 +14,5 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 DATABASES = {
-    'default': {   #dj_database_url.config() --> for Heroku server and does not need below. below is for digitalOcean.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
-    }
+    'default': dj_database_url.config()
 }
-
-# Below for 3rd party auth Key ...etc
