a5c037bd833490f40252d620c1dfebf970247ecd
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-06-11 00:00:08 +0900

    init personal-porfolio

--- wagtail-react/settings/base.py
+++ wagtail-react/settings/base.py
@@ -92,7 +92,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WAGTAIL_SITE_NAME = 'wagtail-react'
 
