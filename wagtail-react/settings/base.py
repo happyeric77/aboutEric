60ca7726d7ce4be612b9dbb0eaf77e72e9d7c661
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-06-11 00:17:33 +0900

    init personal-porfolio

--- wagtail-react/settings/base.py
+++ wagtail-react/settings/base.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'wagtail-react.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- wagtail-react/urls.py
+++ wagtail-react/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .views import dubuggerTest
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -8,6 +8,7 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from django.conf.urls.static import static
 from project.views import sendGridEmail
+from django.views.generic import TemplateView
 
 from .api import api_router
 
@@ -19,7 +20,8 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
     path('pages/', include(wagtail_urls)),
     path('api/v2/', api_router.urls),
-    path('api/email/', sendGridEmail)
+    path('api/email/', sendGridEmail),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
