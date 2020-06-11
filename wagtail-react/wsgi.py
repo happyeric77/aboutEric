5cb174f727897a9dfe04f4483d478fab70b0226a
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-06-12 00:20:33 +0900

    back to no cloudinary

--- wagtail-react/wsgi.py
+++ wagtail-react/wsgi.py
@@ -11,7 +11,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wagtail-react.settings.production')
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
+application = WhiteNoise(get_wsgi_application(), root='/build/static')
