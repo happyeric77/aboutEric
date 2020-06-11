50a1d281cc38a5588fea595aab855d6ad20b6037
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-06-11 13:06:23 +0900

    api mapping

--- src/components/CarouselImg.js
+++ src/components/CarouselImg.js
@@ -8,7 +8,7 @@ const CarouselImg = (props) => {
 
 
     useEffect(()=>{
-        axios.get('http://127.0.0.1:8000/api/v2/pages/4/')
+        axios.get('http://personal-porfolio.herokuapp.com/api/v2/pages/3/')
             .then(res=>{
                 const newApi = {...api, items: res.data.childPages};
                 setApi(newApi);
